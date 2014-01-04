a8556d1f574c860486448326ca212d5f2b941af7
László Monda <laci@monda.hu>
2014-01-04 19:08:29 +0100

    Make the script able to handle a list of modules.

--- rebuild-modules.sh
+++ rebuild-modules.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -e
+
+native_modules="usb"
 
 node_webkit_version=`cat node-webkit.version`
-cd node_modules/usb
-nw-gyp configure --target=$node_webkit_version &&
-nw-gyp build
+for native_module in $native_modules; do
+    echo "Rebuilding $native_module module..."
+    cd node_modules/$native_module
+    nw-gyp configure --target=$node_webkit_version
+    nw-gyp build
+    cd ../..
+done
