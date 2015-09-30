70fe81489654bc33a1f76efd5a7e602220727fbc
László Monda <laci@monda.hu>
2015-09-30 02:44:32 +0200

    Put text into keys upon clicking on them.

--- gui/index.js
+++ gui/index.js
@@ -2,4 +2,14 @@ Snap.load('layer.svg', function(loadedFragment) {
     console.log(loadedFragment);
     var svg = Snap('.layer-container');
     svg.append(loadedFragment);
+    svg.selectAll('.key').forEach(function(element) {
+        element.click(function() {
+            var boundingBox = this.node.getBoundingClientRect();
+            centerY = boundingBox.top + boundingBox.height/2;
+            console.log('click', boundingBox);
+            svg.select('svg').text(boundingBox.left, centerY, 'lo!').attr({fill:'white'});
+        })
+    });
+
+
 });
